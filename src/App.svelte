02b7bc2dd0dcbeba7d34244ff8037576ae06ae6d
Nicole Lee <nicolelee2001@gmail.com>
2023-05-05 04:11:27 +0900

    Create initial p5.js sketch with Nubzuk image

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,34 @@
 <script>
-	export let name;
+  import P5 from "p5-svelte";
+
+  import Nubzuk from "./lib/assets/Nubzuk.png";
+
+  let nubzuk;
+  const sketch = (p5) => {
+    p5.preload = () => {
+      nubzuk = p5.loadImage(Nubzuk);
+    };
+    p5.setup = () => {
+      p5.createCanvas(p5.windowWidth, p5.windowHeight);
+    };
+    p5.draw = () => {
+      p5.background(255);
+
+      p5.imageMode(p5.CENTER);
+      p5.image(nubzuk, p5.width / 2, p5.height - 50, 100, 100);
+    };
+    p5.windowResized = () => {
+      p5.resizeCanvas(p5.windowWidth, p5.windowHeight);
+    };
+  };
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <P5 {sketch} />
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  main {
+    text-align: center;
+  }
 </style>
