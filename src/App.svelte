e15e862549410713441646d8a9ebd6e10c2891ca
Nicole Lee <nicolelee2001@gmail.com>
2023-05-11 05:08:22 +0900

    Move Nubzuk sideways using arrow keys

--- src/App.svelte
+++ src/App.svelte
@@ -22,6 +22,13 @@
     nubzukVy -= g * dt;
   };
 
+  const move = (p5) => {
+    const step = 5;
+    if (p5.keyIsDown(p5.LEFT_ARROW)) nubzukX -= step;
+    if (p5.keyIsDown(p5.RIGHT_ARROW)) nubzukX += step;
+    nubzukX = (nubzukX + p5.width) % p5.width;
+  };
+
   const sketch = (p5) => {
     p5.preload = () => {
       nubzuk = p5.loadImage(Nubzuk);
@@ -40,6 +47,7 @@
       p5.image(nubzuk, nubzukX, p5.height - nubzukY - 50, 100, 100);
 
       jump();
+      move(p5);
     };
     p5.windowResized = () => {
       p5.resizeCanvas(p5.windowWidth, p5.windowHeight);
