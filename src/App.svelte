f5407073645c2e136370186e8edce920d2f72b5a
Nicole Lee <nicolelee2001@gmail.com>
2023-05-11 04:29:16 +0900

    Basic Nubzuk jump movement

--- src/App.svelte
+++ src/App.svelte
@@ -4,18 +4,36 @@
   import Nubzuk from "./lib/assets/Nubzuk.png";
 
   let nubzuk;
+  let nubzukX, nubzukY;
+
+  let jumpStart, jumpDir;
+  const jump = () => {
+    const jumpHeight = 100;
+    if (nubzukY >= jumpStart) jumpDir = -1;
+    if (nubzukY <= jumpStart - jumpHeight) jumpDir = 1;
+    nubzukY += jumpDir;
+  };
+
   const sketch = (p5) => {
     p5.preload = () => {
       nubzuk = p5.loadImage(Nubzuk);
     };
     p5.setup = () => {
       p5.createCanvas(p5.windowWidth, p5.windowHeight);
+
+      nubzukX = p5.width / 2;
+      nubzukY = p5.height;
+
+      jumpStart = nubzukY;
+      jumpDir = 1;
     };
     p5.draw = () => {
       p5.background(255);
 
       p5.imageMode(p5.CENTER);
-      p5.image(nubzuk, p5.width / 2, p5.height - 50, 100, 100);
+      p5.image(nubzuk, nubzukX, nubzukY - 50, 100, 100);
+
+      jump();
     };
     p5.windowResized = () => {
       p5.resizeCanvas(p5.windowWidth, p5.windowHeight);
