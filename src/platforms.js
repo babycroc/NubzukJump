7b47f7b796b9f26188e98c8f88203e59df2763db
Nicole Lee <nicolelee2001@gmail.com>
2023-05-15 14:53:35 +0900

    Guarantee platforms to be created evenly in the vertical space

--- src/platforms.js
+++ src/platforms.js
@@ -5,10 +5,13 @@ const PLATFORM_MAX_HEIGHT = 25;
 const PLATFORM_MIN_DISTANCE = 10;
 
 let platforms = [];
-const initPlatformNum = 20;
 
 const getRandom = (min, max) => {
   return min + Math.random() * (max - min);
+};
+
+export const getPlatforms = () => {
+  return platforms;
 };
 
 export const createPlatforms = (p5) => {
@@ -27,20 +30,34 @@ export const createPlatforms = (p5) => {
   };
 
   const createPlatform = (p5) => {
-    const width = getRandom(PLATFORM_MIN_WIDTH, PLATFORM_MAX_WIDTH);
-    const height = getRandom(PLATFORM_MIN_HEIGHT, PLATFORM_MAX_HEIGHT);
-    const x = getRandom(0, p5.width - width);
-    const y = getRandom(0, p5.height - height);
-    const newPlatform = { x, y, width, height };
-    if (
-      platforms
-        .map((p) => isOverlapping(p, newPlatform))
-        .filter((p) => p == true).length == 0
-    )
-      platforms.push(newPlatform);
+    const yInterval = 100;
+    const platformPerInterval = 3;
+    for (let y1 = 0; y1 <= p5.height - yInterval; y1 += yInterval) {
+      const y2 = y1 + yInterval;
+
+      for (let i = 0; i < platformPerInterval; i++) {
+        const width = Math.floor(
+          getRandom(PLATFORM_MIN_WIDTH, PLATFORM_MAX_WIDTH)
+        );
+        const height = Math.floor(
+          getRandom(PLATFORM_MIN_HEIGHT, PLATFORM_MAX_HEIGHT)
+        );
+        const x = Math.floor(getRandom(0, p5.width - width));
+        const y = Math.floor(getRandom(y1, y2));
+
+        const newPlatform = { x, y, width, height };
+        if (
+          platforms
+            .map((p) => isOverlapping(p, newPlatform))
+            .filter((p) => p == true).length == 0
+        ) {
+          platforms.push(newPlatform);
+        }
+      }
+    }
   };
 
-  while (platforms.length < initPlatformNum) createPlatform(p5);
+  createPlatform(p5);
 };
 
 export const drawPlatforms = (p5) => {
