57b4656e038764c26b64cd2559ca90fbdadd8d6a
Nicole Lee <nicolelee2001@gmail.com>
2023-05-19 17:03:16 +0900

    Edit platform shape

--- src/constants.js
+++ src/constants.js
@@ -12,8 +12,8 @@ export const OBJECT_MIN_DISTANCE = 30;
 
 export const PLATFORM_MIN_WIDTH = 80;
 export const PLATFORM_MAX_WIDTH = 120;
-export const PLATFORM_MIN_HEIGHT = 15;
-export const PLATFORM_MAX_HEIGHT = 20;
+export const PLATFORM_MIN_HEIGHT = 20;
+export const PLATFORM_MAX_HEIGHT = 30;
 
 export const SCORE_DISPLAY_WIDTH = 150;
 export const SCORE_DISPLAY_HEIGHT = 50;
